c69e51d872f773ec778b92a46c4687746c6986bb
Nick Engelhardt <enickgoku2@gmail.com>
2022-07-30 18:15:18 -0400

    added form for EditDeck

--- src/Layout/Components/EditDeckScreen.js
+++ src/Layout/Components/EditDeckScreen.js
@@ -7,6 +7,9 @@ function EditDeckScreen() {
   const { deckId } = useParams()
   const [deck, setDeck] = useState([])
 
+  const placeHolderOne = deck?.name
+  const placeHolderTwo = deck?.description
+
   useEffect(() => {
     const getDeck = async () => {
 
@@ -23,7 +26,25 @@ function EditDeckScreen() {
   return (
     <>
       <Navbar currentDeck={deck} />
-      <h1>Hi</h1>
+      <h1>Edit Deck</h1>
+      <form>
+      <div className='d-flex justify-content-center'>
+        <div className="form-group">
+          <label htmlFor='name'>Name</label>
+          <input type="name" className="form-control" id="name" placeholder={placeHolderOne}/>
+          </div>
+        </div>
+      <div className='d-flex justify-content-center'>
+        <div class="form-group">
+        <label htmlFor='description'>Description</label>
+        <textarea class="form-control" id="description" rows="3" placeholder={placeHolderTwo}></textarea>
+        </div>
+      </div>
+      </form>
+      <div className='d-flex justify-content-start'>
+        <button className="btn btn-secondary" type="cancel">Cancel</button>
+        <button className="btn btn-primary" type="submit">Submit</button>
+      </div>
     </>
   )
 }
