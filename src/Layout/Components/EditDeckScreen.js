ce4b5ce5437960b1bf3d092c001d319b7200248b
Nick Engelhardt <enickgoku2@gmail.com>
2022-07-30 18:52:43 -0400

    added change handlers

--- src/Layout/Components/EditDeckScreen.js
+++ src/Layout/Components/EditDeckScreen.js
@@ -1,11 +1,32 @@
 import Navbar from '../Navbar'
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import { readDeck } from '../../utils/api/index'
+import { readDeck, updateDeck } from '../../utils/api/index'
 
 function EditDeckScreen() {
   const { deckId } = useParams()
   const [deck, setDeck] = useState([])
+
+  const initialFormData = {
+    name: "",
+    description: "",
+  }
+
+  const [formData, setFormData] = useState(initialFormData)
+  
+  const handleChange = ({ target }) => {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    updateDeck(formData)
+    setFormData(initialFormData)
+  }
+
 
   const placeHolderOne = deck?.name
   const placeHolderTwo = deck?.description
@@ -27,14 +48,14 @@ function EditDeckScreen() {
     <>
       <Navbar currentDeck={deck} />
       <h1>Edit Deck</h1>
-      <form className='form-group'>
+      <form className='form-group' onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor='name'>Name</label>
-            <input type="name" className="form-control form-control-lg" id="name" placeholder={placeHolderOne}/>
+            <input type="name" className="form-control form-control-lg" id="name" placeholder={placeHolderOne} value={formData.name} onChange={handleChange}/>
           </div>
           <div class="form-group">
             <label htmlFor='description'>Description</label>
-            <textarea className="form-control form-control-lg" id="description" rows="3" placeholder={placeHolderTwo}></textarea>
+            <textarea className="form-control form-control-lg" id="description" rows="3" placeholder={placeHolderTwo} value={formData.description} onChange={handleChange}></textarea>
           </div>
       </form>
         <div className='d-flex'>
