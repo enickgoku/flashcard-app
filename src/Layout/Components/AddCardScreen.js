884071ecb602f2cc3ccfa431cd571651718e1744
Nick Engelhardt <enickgoku2@gmail.com>
2022-07-31 14:51:17 -0400

    submit and change handlers on addcardscreen

--- src/Layout/Components/AddCardScreen.js
+++ src/Layout/Components/AddCardScreen.js
@@ -1,11 +1,12 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import { readDeck } from '../../utils/api'
+import { readDeck, createCard } from '../../utils/api'
 import Navbar from '../Navbar'
 
 function AddCardScreen() {
   const { deckId } = useParams()
   const [deck, setDeck] = useState([])
+  const history = useHistory()
 
   const initialFormData = {
     front: "",
@@ -26,18 +27,31 @@ function AddCardScreen() {
     getDeckInfo()
   }, [deckId])
 
+  const handleChange = async ({ target }) => {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    createCard(deckId, formData)
+    .then(setFormData(initialFormData))
+  }
+
   return (
     <>
       <Navbar currentDeck={deck} />
       <h1>{deck.name}: Add Card</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor='front'>Back</label>
-          <textarea type="text" name="front" className="form-control form-control-lg" id="front" placeholder="Front side of card." value={formData.front}/>
+          <label htmlFor='front'>Front</label>
+          <textarea type="text" name="front" className="form-control form-control-lg" id="front" placeholder="Front side of card." value={formData.front} onChange={handleChange}/>
         </div>
         <div class="form-group">
           <label htmlFor='back'>Back</label>
-          <textarea type="text" name="back" className="form-control form-control-lg" id="back" rows="3" placeholder="Back side of card." value={formData.back}/>
+          <textarea type="text" name="back" className="form-control form-control-lg" id="back" rows="3" placeholder="Back side of card." value={formData.back} onChange={handleChange}/>
         </div>
         <div className='d-flex'>
           <a href={`/decks/${deckId}`} className="btn btn-secondary mr-1" type="cancel">Cancel</a>
