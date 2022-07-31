acf83b013752b755eafecf063a9a9025dc0481db
Nick Engelhardt <enickgoku2@gmail.com>
2022-07-31 11:28:46 -0400

    fixed navbar conditional render

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -3,12 +3,15 @@ import { useRouteMatch, Link, useParams } from 'react-router-dom'
 function Navbar({ currentDeck = {} }) {
   const match = useRouteMatch()
   const path = match.path
+  const pathMatch = !path.includes('/study')
+  const pathMatchTwo = !path.includes('/edit')
+  const pathMatchThree = !path.includes('/new')
   const { deckId } = useParams()
 
 
   return (
     <>
-      { deckId && !path.match(/deck/)  ? (
+      { pathMatch && pathMatchTwo && pathMatchThree ? (
       <nav aria-label="breadcrumb">
         <ol className="breadcrumb">
           <li className="breadcrumb-item"><Link to="/"><span className="oi oi-home"></span> Home</Link></li>
