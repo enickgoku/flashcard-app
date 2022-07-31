eef5fa4e310ab668ca78ea0df81b8151d52c3f41
Nick Engelhardt <enickgoku2@gmail.com>
2022-07-31 13:26:29 -0400

    added form on addCardScreen

--- src/Layout/Components/AddCardScreen.js
+++ src/Layout/Components/AddCardScreen.js
@@ -7,10 +7,21 @@ function AddCardScreen() {
   const { deckId } = useParams()
   const [deck, setDeck] = useState([])
 
+  const initialFormData = {
+    front: "",
+    back: "",
+  }
+
+  const [formData, setFormData] = useState(initialFormData)
+
   useEffect(() => {
     const getDeckInfo = async () => {
-      const response = await readDeck(deckId)
-      return setDeck(response)
+      try{
+        const response = await readDeck(deckId)
+        return setDeck(response)
+      } catch (error) {
+        console.log(error)
+      }
     }
     getDeckInfo()
   }, [deckId])
@@ -19,6 +30,20 @@ function AddCardScreen() {
     <>
       <Navbar currentDeck={deck} />
       <h1>{deck.name}: Add Card</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor='front'>Back</label>
+          <textarea type="text" name="front" className="form-control form-control-lg" id="front" placeholder="Front side of card." value={formData.front}/>
+        </div>
+        <div class="form-group">
+          <label htmlFor='back'>Back</label>
+          <textarea type="text" name="back" className="form-control form-control-lg" id="back" rows="3" placeholder="Back side of card." value={formData.back}/>
+        </div>
+        <div className='d-flex'>
+          <a href={`/decks/${deckId}`} className="btn btn-secondary mr-1" type="cancel">Cancel</a>
+          <button className="btn btn-primary" type="submit">Save</button>
+        </div>
+      </form>
     </>
   )
 }
