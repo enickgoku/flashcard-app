05341fab9907c26e8fbb14805cffdc8bf3f49a56
Nick Engelhardt <enickgoku2@gmail.com>
2022-08-01 19:30:09 -0400

    add card screen

--- src/Layout/Components/EditCardScreen.js
+++ src/Layout/Components/EditCardScreen.js
@@ -1,6 +1,49 @@
+import { useEffect, useState } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import { readCard, readDeck } from '../../utils/api/index'
+import Navbar from '../Navbar'
+
 function EditCardScreen() {
+  const { deckId, cardId } = useParams()
+  const history = useHistory()
+  const [currentDeck, setCurrentDeck] = useState([])
+  const [card, setCard] = useState([])
+
+  useEffect(() => {
+    const getDeck = async () => {
+      const response = await readDeck(deckId)
+      setCurrentDeck(response)
+    }
+    getDeck()
+  }, [deckId])
+
+  useEffect(() => {
+    const getCard = async () => {
+      const currentCard = await readCard(cardId)
+      setCard(currentCard)
+    }
+    getCard()
+  }, [cardId])
+
   return (
-    <h1>Hi</h1>
+    <>
+      <Navbar currentDeck={currentDeck} />
+      <h1>Edit Card</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor='front'>Front</label>
+          <textarea type="text" name="front" className="form-control form-control-lg" id="front" placeholder={card.front}/>
+        </div>
+        <div class="form-group">
+          <label htmlFor='back'>Back</label>
+          <textarea type="text" name="back" className="form-control form-control-lg" id="back" rows="3" placeholder={card.back}/>
+        </div>
+        <div className='d-flex'>
+          <a href={`/decks/${deckId}`} className="btn btn-secondary mr-1" type="cancel">Cancel</a>
+          <button className="btn btn-primary" type="submit">Save</button>
+        </div>
+      </form>
+    </>
   )
 }
 
--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,12 +1,13 @@
 import { useRouteMatch, Link, useParams } from 'react-router-dom'
 
 function Navbar({ currentDeck = {} }) {
+  const { deckId } = useParams()
   const match = useRouteMatch()
   const path = match.path
   const pathMatch = !path.includes('/study')
   const pathMatchTwo = !path.includes('/edit')
   const pathMatchThree = !path.includes('/new')
-  const { deckId } = useParams()
+  const pathMatchFour = path.includes('/cards')
 
 
   return (
@@ -23,7 +24,11 @@ function Navbar({ currentDeck = {} }) {
         <ol className="breadcrumb">
           <li className="breadcrumb-item"><Link to="/"><span className="oi oi-home"></span> Home</Link></li>
           <li className="breadcrumb-item"><Link to={`/decks/${deckId}`}> {currentDeck?.name}</Link></li>
-          <li className="breadcrumb-item active" aria-current="page"> Edit Deck</li>
+          {pathMatchFour && !pathMatchTwo ? (
+            <li className="breadcrumb-item active" aria-current="page"> Edit Card</li>
+          ) : (
+            <li className="breadcrumb-item active" aria-current="page"> Edit Deck</li>
+          )}
         </ol>
       </nav>
       )}
