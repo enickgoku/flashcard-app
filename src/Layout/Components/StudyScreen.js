432310b164032940c28081ecda00ac0f946819d6
Nick Engelhardt <enickgoku2@gmail.com>
2022-08-02 13:21:08 -0400

    started study screen

--- src/Layout/Components/StudyScreen.js
+++ src/Layout/Components/StudyScreen.js
@@ -1,12 +1,33 @@
-// import { useEffect } from 'react'
-// import { useParams, useRouteMatch } from 'react-router-dom'
-// import { readDeck } from '../../utils/api/index'
+import { useEffect, useState, Link } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import { readDeck } from '../../utils/api/index'
 
-function StudyScreen({ decks }) {
-  
+function StudyScreen() {
+  const history = useHistory()
+  const { deckId } = useParams()
+  const [deck, setDeck] = useState([])
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    const getAndSetDeck = async () => {
+      const response = await readDeck(deckId)
+      const isValid = response.cards.length >= 3
+      setDeck({
+        ...response,
+        isValid: isValid,
+      })
+      setCards({
+        ...response.cards,
+        isFlipped: false,
+      })
+    }
+    getAndSetDeck()
+  }, [deckId])
+
+
   return (
     <>
-      <h1>Working</h1>
+      <h1>Study: {deck.name}</h1>
     </>
   )
 }
